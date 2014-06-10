8a7a8591ddff3e3ec52e068e1abf9e9477fab2ff
jeb <jeb@localhost.localdomain>
2014-06-10 09:15:59 -0500

    moving setup file

--- vim-setup.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo yum install ack
-vim +PluginInstall +qall
-cd ~/.vim/bundle/vimproc.vim
-make
-sudo mv /usr/share/vim/vim74/ftplugin/lisp.vim ~/lisp.vim.backup
