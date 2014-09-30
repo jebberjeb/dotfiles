cb48bab8cd7b5b5cf3e3483dddb3f4861d4a65c1
Relevance Pair <pair@thinkrelevance.com>
2014-09-30 11:35:04 -0400

    adding c.t.n, ring-mock to lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-immutant "1.2.0"]
                   [lein-pprint "1.1.1"]]
-        :dependencies [[org.clojure/tools.namespace "0.2.7"]]
+        :dependencies [[org.clojure/tools.namespace "0.2.7"]
+                       [ring-mock                   "0.1.5"]]
         :ring {:nrepl {:start? true :port 50000}}}}
