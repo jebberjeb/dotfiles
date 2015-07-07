e9ff1193d87794e7e2a15d9740cc9bf9b581aa56
Jeb Beich <jebbeich@gmail.com>
2015-07-07 07:46:24 -0500

    Updated cidr-nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,6 @@
         :dependencies [[org.clojure/tools.namespace "0.2.7"]
                        [ring-mock                   "0.1.5"]
                        [com.taoensso/timbre         "3.3.1"]
-                       [cider/cider-nrepl           "0.8.1"]
+                       [cider/cider-nrepl           "0.9.1"]
                        [grimvim                     "0.1.0"]]
         :ring {:nrepl {:start? true :port 50000}}}}
